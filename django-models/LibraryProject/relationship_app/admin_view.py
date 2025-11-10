4fcc2fb1df5014072f76f871122ae573558aa3ba
Hilda Enyioko <enyiokohilda@gmail.com>
2025-11-10 08:40:29 +0100

    check

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,9 +1,8 @@
-# An ‘Admin’ view that only users with the ‘Admin’ role can access
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .utils import role_required
+from relationship_app.utils import role_required
 
 @login_required
 @role_required('Admin')
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, 'relationship_app/admin_view.html', {})
