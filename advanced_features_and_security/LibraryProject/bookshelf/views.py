e96a1fefda71f04e86bf458430a8e50ddc0b5540
Hilda Enyioko <enyiokohilda@gmail.com>
2025-11-18 19:14:48 +0100

    forms.py updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 from django.views.decorators.csrf import csrf_protect
 
 def search_books(request):
