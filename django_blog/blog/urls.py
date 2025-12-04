005c32ff0878f7af81c51b12a747ce581337b793
Hilda Enyioko <enyiokohilda@gmail.com>
2025-12-04 09:54:55 +0100

    edited comment urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-    path("post/<int:post_id>/comments/new/", CommentCreateView, name="comment-add"),
-    path("comment/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-update"),
+    path("post/<int:pk>/comments/new/", CommentCreateView, name="comment-add"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
